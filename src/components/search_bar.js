1d5dcac479c87e35150095d81afa8b688b91cebc
fzoozai <fabian.soosaithasan@gmail.com>
2016-03-16 09:05:48 +0100

    In order to interact with components, use the react component onChange.

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 
-const SearchBar = () => {
-    return <input />
-};
+class SearchBar extends React.Component {
+    render() {
+      return <input onChange = {this.onInputChange} />;
+    }
+
+    onInputChange(event) {
+        console.log(event.target.value);
+    }
+}
 
 export default SearchBar;
